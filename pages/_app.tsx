f2b408b428ec2ff279b65f3df06b1f0274339985
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-01-25 23:12:57 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
-import { Montserrat } from '@next/font/google'
-import Head from 'next/head';
-import '../styles/global.css'
+import { Montserrat } from "@next/font/google";
+import Head from "next/head";
+import "../styles/global.css";
 
 const montserrat = Montserrat({
-    subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 const _App = (props: any) => {
   const { Component, pageProps } = props;
   return (
     <main className={montserrat.className}>
       <Head>
-        <link rel='icon' href='./boffocake-logo.png'/>
+        <link rel="icon" href="./boffocake-logo.png" />
       </Head>
       <Component {...pageProps} />
     </main>
-  )
-}
+  );
+};
 
-export default _App
+export default _App;
