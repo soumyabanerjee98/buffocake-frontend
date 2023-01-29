e0ff0b9dcf57fa46f83c4451b8b3b24f4d579194
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-01-29 15:25:43 +0530

    Create processID.tsx

---
+++ config/processID.tsx
@@ -0,0 +1,13 @@
+export const processIDs = {
+  phone_verify: "phone-verify",
+  otp_verify: "otp-verify",
+  verify_login_token: "verify-login-token",
+  user_login_with_phone: "user-login-with-phone",
+  user_login_with_email: "user-login-with-email",
+  user_email_check: "user-email-check",
+  user_phone_check: "user-phone-check",
+  create_new_account: "create-new-account",
+  paytm_transaction_token_generate: "paytm-transaction-token-generate",
+  get_all_products: "get-all-products",
+  get_product_details: "get-product-details",
+};
