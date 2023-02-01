a775e842e3cee40b07913cd4be3ab6cb5d116623
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-02-01 19:36:22 +0530

    Revert "Update Products.tsx"
    This reverts commit fc58ed3b2c284134bffa438b8650ce08d4eb74d0.

--- projectComponents/Pages/Products.tsx
+++ projectComponents/Pages/Products.tsx
@@ -299,7 +299,7 @@ const Products = (props: ProductProps) => {
             className="product-image"
           />
         )}
-        {isLoading ? null : (
+        {isLoading === false && (
           <div className="fav" onClick={favSelect}>
             <HeartIcon className="heart" fill={fav ? "red" : "white"} />
           </div>
