892a655a4dc57533fa042a845a4772796b698abb
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-02-03 17:35:09 +0530

    Revert "Update layout.tsx"
    This reverts commit 02f28d480169930879893f0e55fa936de2ae5438.

--- app/product/[slug]/layout.tsx
+++ app/product/[slug]/layout.tsx
@@ -3,11 +3,11 @@ import ProductPageLoading from "./loading";
 
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <main>
       <React.Suspense fallback={<ProductPageLoading />}>
         {children}
       </React.Suspense>
-    </>
+    </main>
   );
 };
 
